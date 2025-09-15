81b92999e5bc051f1c6b770ad99345638b09a14f
Faizimughal <162406605+Faizimughal@users.noreply.github.com>
2025-09-15 22:32:03 +0500

    page 3 added

--- assets/js/page2.js
+++ assets/js/page2.js
@@ -1,5 +1,6 @@
 let answers = document.querySelectorAll(".answer");
 let buttons = document.querySelectorAll(".arrow-btn");
+let questions = document.querySelectorAll(".question1");
 
 window.addEventListener("DOMContentLoaded", () => {
   answers.forEach((answer) => {
@@ -13,10 +14,26 @@ buttons.forEach((button, index) => {
       if (i !== index) {
         answer.classList.add("none");
         buttons[i].classList.remove("rotate");
+        questions[i].style.marginTop = ""; // reset margin
       }
     });
 
     answers[index].classList.toggle("none");
     button.classList.toggle("rotate");
+
+    if (!answers[index].classList.contains("none")) {
+      questions[index].style.marginTop = "0";
+
+      // pehle answer open hone de, phir scroll kare
+      setTimeout(() => {
+        let top = questions[index].getBoundingClientRect().top + window.scrollY;
+        window.scrollTo({
+          top: top - 100, // thoda gap de upar
+          behavior: "smooth",
+        });
+      }, 500); // delay taake answer expand ho jaye
+    } else {
+      questions[index].style.marginTop = "";
+    }
   });
 });
